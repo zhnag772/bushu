1083b81bf75a8484606ee5cb833587e7674fb7e7
zhnag772 <48310927+zhnag772@users.noreply.github.com>
2026-02-02 23:22:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,10 +51,12 @@ curl -fsSL https://raw.githubusercontent.com/zhnag772/bushu/refs/heads/main/mail
 # =========================
 if ! command -v docker &>/dev/null; then
     echo -e "安装 Docker..."
+    apt-get update
     apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
         > /etc/apt/sources.list.d/docker.list
+    apt-get update
     apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
